7aed5cf812c41ee8f732c4453359212e4f952abf
shubhamggosai <63947251+shubhamggosai@users.noreply.github.com>
2020-09-11 19:45:36 +0530

    Create update.sh

---
+++ ShuBham/update.sh
@@ -0,0 +1,51 @@
+Skip to content
+Shubhamggosai / virous
+#colour section
+red='\033[1;31m'
+rset='\033[0m'
+grn='\033[1;32m'
+ylo='\033[1;33m'
+#script coding starts
+clear
+echo " "
+echo " "
+echo '
+                  _  _  ___  ___   __  ____  ___ 
+                 ( )( )(  ,\(   \ (  )(_  _)(  _)
+                  )()(  ) _/ ) ) )/__\  )(   ) _)
+                  \__/ (_)  (___/(_)(_)(__) (___) v 1.0
+'|lolcat
+echo " "
+echo " "
+sleep 6.0
+clear
+echo -e "$red                         ChEcKiNg..>$rset"
+sleep 2.0
+clear
+echo -e "$red                         ChEcKiNg...>$rset"
+sleep 2.0
+clear
+echo -e "$red                         ChEcKiNg....>$rset"
+sleep 2.0
+clear
+echo -e "$red                         ChEcKiNg.....>$rset"
+sleep 2.0
+clear
+echo " "
+echo " "
+echo -e "$grn                updating the virous  please wait$rset"
+sleep 2.0
+cd $HOME
+rm -rf virous
+git clone https://github.com/shubhamggosai/virous
+clear
+echo " "
+echo -e "$grn               virous has been updated check it now...$rset"
+sleep 3.0
+echo " "
+cd $HOME
+cd virous
+bash virous.sh
+clear
+sleep 1.0
+
